7caf6d8526d1902c3765f4242be511a32c1ec321
carlosantonioni <carant06@gmail.com>
2021-05-16 04:17:58 -0400

    refactored domain to model

---
+++ src/main/java/com/SpringBoot/Employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.SpringBoot.Employeemanager.repo;
+import com.SpringBoot.Employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
+}
